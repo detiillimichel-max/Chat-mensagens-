5f0eab3133256da20d5a456ddc0bd4818e612d1b
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 19:30:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,40 @@
-const CACHE = "vibe-cache";
+const CACHE = "vibe-cache-v1";
 
+// Arquivos que serão guardados no celular para o app abrir rápido
+const assets = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png" // Garanta que subiu esse arquivo no GitHub
+];
+
+// Instalação do Service Worker
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./css/style.css",
-        "./js/script.js"
-      ]);
+      console.log("Arquivos cacheados com sucesso!");
+      return cache.addAll(assets);
     })
   );
 });
 
+// Ativação e limpeza de caches antigos
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Responde com o cache ou busca na rede
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
