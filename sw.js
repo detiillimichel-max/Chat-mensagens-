86c8050b9166d8439b4fc063050f303c892238e2
Michel Detiilli <detiillimichel@gmail.com>
2026-04-05 23:51:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = 'vibe-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
 
+// Instalação do Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Responde com o Cache quando estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
